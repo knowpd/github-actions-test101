f07b27759467e00d5b56c079e1b388f1cf34c16e
Hee Won Lee <knowpd@gmail.com>
2025-05-25 18:32:47 -0400

    ADD helloworld/server.js

---
+++ helloworld/server.js
@@ -0,0 +1,19 @@
+// 2022-06-03 knowpd
+// <https://nodejs.org/en/docs/guides/getting-started-guide/>
+
+const http = require('http');
+
+const hostname = '';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+  console.log('request: ', req.url);
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World')
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at port ${port}`);
+});
